f806bbb88e26915e39748f2a478a6e572d30a7ed
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-03 17:03:54 +0200

    Update appmetadata.js

--- plugins/appmetadata.js
+++ plugins/appmetadata.js
@@ -3,25 +3,27 @@ export default async function ({ app: { $auth, $axios, $config: { browserBaseURL
   if (!$auth.loggedIn) {
     return
   }
-  const { email } = $auth.$storage.getUniversal('user')
+  if($auth) {
+    const { email } = $auth.$storage.getUniversal('user')
   
-  //const appMetaData = $auth.$storage.getUniversal('appMetaData')
-  //if(!appMetaData) {
-    /*const headers = {
-      'content-type': 'application/x-www-form-urlencoded',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
-      'Access-Control-Allow-Headers': 'append,delete,entries,foreach,get,has,keys,set,values,Authorization',
-      //'RewriteEngine': 'On',
-      //'RewriteCond': '%{REQUEST_METHOD} OPTIONS',
-      //'RewriteRule': '^(.*)$ $1 [R=200,L]'
-    }*/
-    try{
-      const { data } = await axios.post('/server/management', email)
-      $auth.$storage.setUniversal('metaData', data, false)
+    //const appMetaData = $auth.$storage.getUniversal('appMetaData')
+    //if(!appMetaData) {
+      /*const headers = {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+        'Access-Control-Allow-Headers': 'append,delete,entries,foreach,get,has,keys,set,values,Authorization',
+        //'RewriteEngine': 'On',
+        //'RewriteCond': '%{REQUEST_METHOD} OPTIONS',
+        //'RewriteRule': '^(.*)$ $1 [R=200,L]'
+      }*/
+      try{
+        const { data } = await axios.post('/server/management', email)
+        $auth.$storage.setUniversal('metaData', data, false)
 
-    } catch (err) {
-      console.log(err)
-    }
-  //}
+      } catch (err) {
+        console.log(err)
+      }
+    //}
+  }
 }
