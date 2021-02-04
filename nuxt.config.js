fbb0ca5d5a5a97ba04cb8346333199d7dcd9207d
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-04 22:33:40 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,7 @@ export default {
   ],
   
   axios: {
-    baseURL: process.env.BASE_URL
+    baseURL: https://yellowe-chat.herokuapp.com
     //browserBaseURL: process.env.BROWSER_BASE_URL,
     //proxy: true,
     //https: true
