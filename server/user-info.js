beb119a1c65cdd645f985be9708e6430dd73ee21
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-07 23:04:48 +0200

    Update user-info.js

--- server/user-info.js
+++ server/user-info.js
@@ -9,44 +9,48 @@ const clientSecret = process.env.AUTH0_CLIENT_SECRET
 app.post('/', async (req, res, next) => {
     const { body: { text, cloudData, email } } = req
     try{
-      const { data: { access_token, token_type } } = await axios.post('https://dev-p69g86kq.us.auth0.com/oauth/token/', {
-        grant_type: 'client_credentials',
-        client_id: clientId,
-        client_secret: clientSecret,
-        audience: 'https://dev-p69g86kq.us.auth0.com/api/v2/'
-      })
-      const { data } = await axios.get(`https://dev-p69g86kq.us.auth0.com/api/v2/users?q=email:"${email}"&search_engine=v3`, {
-        headers: {
-          "authorization": `${token_type} ${access_token}`
-        }
-      })
-      if(data[0]) {
-        const user_id = data[0].user_id
-      
-        if(user_id) {
-          var options = {
-            method: 'PATCH',
-            url: `https://dev-p69g86kq.us.auth0.com/api/v2/users/${user_id}`,
-            headers: {authorization: `${token_type} ${access_token}`, 'content-type': 'application/json'},
-            data: {
-              user_metadata: {
-                  info: text,
-                  cloudData: cloudData
-              }
-            }
-          };      
-          
-          try {
-            const { data: { user_metadata } } = await axios.request(options)
-            res.json(user_metadata)
-            //next()
-          } catch(err) {
-            console.log(err)
-          }
-        }
+      var options = { method: 'POST',
+        url: 'https://dev-p69g86kq.us.auth0.com/oauth/token',
+        headers: { 'content-type': 'application/json' },
+        body: '{"client_id":"y4W7sXKqe6pOd6wxgRlbm2syLcZ9Zes4","client_secret":"4Umk4WQj6eM16hMLhrhNB5fav9RplvKwUWePYFDMjWJgUpOaQRAWjAOyb2nTU-N8","audience":"https://dev-p69g86kq.us.auth0.com/api/v2/","grant_type":"client_credentials"}'
       }
+
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error)
+        const token_type = body.token_type
+        const access_token = body.access_token
+        var options1 = { method: 'GET',
+          url: `https://dev-p69g86kq.us.auth0.com/api/v2/users?q=email:"${email}"&search_engine=v3`,
+          headers: { authorization: `${token_type} ${access_token}` }
+        }
       
+        request(options1, function (error, response, body) {
+          if (error) throw new Error(error)
+          const data = JSON.parse(body)
+          if(data[0]) {
+            const user_id = data[0].user_id
       
+            if(user_id) {
+              var options2 = {
+                method: 'PATCH',
+                url: `https://dev-p69g86kq.us.auth0.com/api/v2/users/${user_id}`,
+                headers: {authorization: `${token_type} ${access_token}`, 'content-type': 'application/json'},
+                body: {
+                  user_metadata: {
+                    info: text,
+                    cloudData: cloudData
+                  }
+                }
+              }  
+    
+              request(options2, function (error, response, body) {
+                if (error) throw new Error(error)
+                const data = JSON.parse(body)
+                return res.json(data[0].user_metadata)
+            }
+          }
+        })
+      })
     }
       catch(err){
         console.log(err)
