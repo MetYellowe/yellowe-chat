1d182a4b2fc4b8a788d6bb7561651c852455270e
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-07 23:33:03 +0200

    Update socket.js

--- plugins/socket.js
+++ plugins/socket.js
@@ -5,7 +5,7 @@ import VueSocketIO from 'vue-socket.io'
 export default function({ store }) {
     Vue.use(new VueSocketIO({
         debug: false,
-        connection: process.env.BASE_URL,
+        connection: `${process.env.BASE_URL}:4113`,
         vuex: {
             store,
             actionPrefix: 'SOCKET_',
