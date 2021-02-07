8d7b6b372ef7bafcb5c52e42b48a28e055a76677
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-07 21:30:38 +0200

    Update appmetadata.js

--- plugins/appmetadata.js
+++ plugins/appmetadata.js
@@ -19,12 +19,12 @@ export default async function ({ app: { $auth, $axios, $config: { browserBaseURL
       }*/
       try{
         const  data  = await $axios.$post('/server/management', { email })
-        const metaData = {
+        /*const metaData = {
           userMetaData: data[0].user_metadata,
           appMetaData: data[0].app_metadata,
           user_id: data[0].user_id
-        }
-        $auth.$storage.setUniversal('metaData', metaData, false)
+        }*/
+        $auth.$storage.setUniversal('metaData', data, false)
 
       } catch (err) {
         console.log(err)
