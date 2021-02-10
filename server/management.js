7b4d553ec838ca7534372381dc7906d7deeac653
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-10 20:21:22 +0200

    Update management.js

--- server/management.js
+++ server/management.js
@@ -8,9 +8,9 @@ app.use(express.json())
 
 const clientId = process.env.AUTH0_CLIENT_ID
 const clientSecret = process.env.AUTH0_CLIENT_SECRET
-const managementAxios = axios.create({
+/*const managementAxios = axios.create({
   baseURL: process.env.AUTH_BASE_URL
-})
+})*/
 
 app.post('/', async (req, res, next) => {
   
