309c123a882ee05640efae864c49b32c458f14d7
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-15 23:30:18 +0200

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const { app, server } = require('./web-socket')
+
+// import and set Nuxt.js options
+let config = require('../nuxt.config')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+async function start() {
+    // init Nuxt.js
+    const nuxt = new Nuxt(config)
+
+    const { host, port } = nuxt.options.server
+
+    // build only in dev mode
+    if(config.dev) {
+        const builder = new Builder(nuxt)
+        await builder.build()
+    } else {
+        await nuxt.ready()
+    }
+
+    // give nuxt middleware to express
+    app.use(nuxt.render)
+
+    // listen the server
+    server.listen(port, () => {
+        consola.ready({
+            message: `Server listening on http://${host}:${port}`,
+            badge: true
+        })
+    })
+}
+
+start()
