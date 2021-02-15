8f6f5f5e1fca074ae9b507909843ef215fb74236
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-16 00:45:06 +0200

    Update socket.js

--- plugins/socket.js
+++ plugins/socket.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
 //import store from '../store'
 //const HOST = location.origin.replace(/^http/, 'ws')
-export default function({ store }) {
+export default function({ store, env }) {
     Vue.use(new VueSocketIO({
         debug: false,
-        connection: `https://yellowe-chat.herokuapp.com:${HEROKU_PORT}`,
+        connection: `https://yellowe-chat.herokuapp.com:${env.HEROKU_PORT}`,
         vuex: {
             store,
             actionPrefix: 'SOCKET_',
