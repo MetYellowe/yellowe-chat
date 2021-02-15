8f082b83ce3b7434ac04748a7e3f40132be05b74
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-15 22:28:07 +0200

    Update web-socket.js

--- server/web-socket.js
+++ server/web-socket.js
@@ -1,8 +1,31 @@
-const app = require('express')
+/*const app = require('express')
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
-const users = require('./users')()
+const users = require('./users')()*/
+const port = process.env.PORT || 3000
+const isProd = process.env.NODE_ENV === 'production'
 
+const http = require('http')
+const app = require('express')()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+const { Nuxt, Builder } = require('nuxt')
+// We instantiate Nuxt with the options
+const config = require('./nuxt.config.js')
+config.dev = !isProd
+
+const nuxt = new Nuxt(config)
+// Start build process in dev mode
+if (config.dev) {
+  const builder = new Builder(nuxt)
+  builder.build()
+}
+app.use(nuxt.render)
+
+// Listen the server
+server.listen(port, '0.0.0.0')
+console.log('Server listening on localhost:' + port) // eslint-disable-line no-console
 const m = (name, text, id, room) => ({name, text, id, room})
 
 io.on('connection', socket => {
