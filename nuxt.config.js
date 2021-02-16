813d05e7d4ca444656a233c5184a84961da0a6f5
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-16 19:14:25 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
-    { src: '@/plugins/socket', ssr: false }
+    //{ src: '@/plugins/socket', ssr: false }
     //'~/plugins/auth0'
   ],
   /*
