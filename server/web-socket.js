c76f3c780cd575e4be2a6d18de5a3389130dc104
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-16 19:04:53 +0200

    Update web-socket.js

--- server/web-socket.js
+++ server/web-socket.js
@@ -1,6 +1,7 @@
-const app = require('express')
-const server = require('http').createServer(app)
-const io = require('socket.io')(process.env.PORT)
+const express = require('express')
+const server = express().listen(process.env.PORT)
+//const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 const users = require('./users')()
 
 io.on('connection', socket => {
