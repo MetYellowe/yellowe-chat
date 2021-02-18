3f9ce0c452ce72e18efcb8f46c69d90028b679b0
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-02-18 23:59:27 +0200

    Update socket.js

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
 //import store from '../store'
-const HOST = location.origin.replace(/^https/, 'wss')
+//const HOST = location.origin.replace(/^https/, 'wss')
 export default function({ store }) {
     Vue.use(new VueSocketIO({
         debug: false,
-        connection: `${HOST}:80/`,
+        connection: `/`,
         vuex: {
             store,
             actionPrefix: 'SOCKET_',
             mutationPrefix: 'SOCKET_'
         },
-        //options: { path: '/server/index' }
+        options: { path: '/server/web-soket' }
     }))
 }
