4d3dd3766775242b45731b70c783cd158fe7a1b3
Alexander <63749001+MetYellowe@users.noreply.github.com>
2021-03-03 16:21:57 +0200

    Update socket.js

--- plugins/socket.js
+++ plugins/socket.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
 //import store from '../store'
 //const HOST = location.origin.replace(/^https/, 'wss')
-/*export default function({ store }) {
+export default function({ store }) {
     Vue.use(new VueSocketIO({
         debug: false,
         connection: 'https://yellowe-chat-project.ew.r.appspot.com',
@@ -14,7 +14,7 @@ import VueSocketIO from 'vue-socket.io'
         //withCredentials: true
         //options: { path: '/' }
     }))
-}*/
+}
 /*const io = require("socket.io-client");
 const socket = io("https://yellowe-chat-project.ew.r.appspot.com", {
   withCredentials: true
